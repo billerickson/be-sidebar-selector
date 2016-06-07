f46ab32be308088ad93baf1e7aa3009c293aced7
Bill Erickson <bill.erickson@gmail.com>
2016-06-07 15:09:04 -0500

    initial commit

---
+++ be-sidebar-selector.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: BE Sidebar Selector
+ * Version: 1.0.0
+ * Description: Create new sidebars, and select which sidebar is used when editing pages
+ * Author: Bill Erickson
+ * Author URI: http://www.billerickson.net
+ * Plugin URI: https://github.com/billerickson/be-sidebar-selector
+ * Text Domain: be-sidebar-selector
+ * Domain Path: /languages
+ * @package BE Sidebar Selector
+ */
